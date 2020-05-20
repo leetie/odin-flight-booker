2bd0df87bc0cd6d6b2ca491653f96a6cfdbfce8a
Jesse Parsons <savvyjesse@aol.com>
2020-05-20 13:25:42 -0400

    fix relation issue. relations now automatically built between Airport and its outbound and inbound flights, and Flight and its to and from with either build, create, or <<

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,6 +1,6 @@
 class Airport < ApplicationRecord
-  has_and_belongs_to_many :outbound_flights, class_name: "Flight", inverse_of: "from"
-  has_and_belongs_to_many :inbound_flights, class_name: "Flight", inverse_of: "to" 
+  has_and_belongs_to_many :outbound_flights, class_name: "Flight", inverse_of: "from", association_foreign_key: "outbound_id"
+  has_and_belongs_to_many :inbound_flights, class_name: "Flight", inverse_of: "to", association_foreign_key: "inbound_id"
   # has_and_belongs_to_many :inbound_flights, class_name: "Flight", inverse_of: "to", foreign_key: "to"
   # has_and_belongs_to_many :outbound_flights, class_name: "Flight", inverse_of: "from", foreign_key: "from"
 end
--- db/migrate/20200520010557_create_airports_flights_join_table.rb
+++ db/migrate/20200520010557_create_airports_flights_join_table.rb
@@ -1,10 +1,15 @@
 class CreateAirportsFlightsJoinTable < ActiveRecord::Migration[6.0]
   def change
     create_table :airports_flights, id: false do |t|
-      t.integer :from_id
-      t.integer :to_id
+      t.integer :outbound_id
+      t.integer :inbound_id
       t.belongs_to :flight
       t.belongs_to :airport
+      
+      # t.integer :from_id
+      # t.integer :to_id
+      # t.belongs_to :flight
+      # t.belongs_to :airport
       # add_index :airports_flights, :from_id
       # add_index :airports_flights, :to_id
     end
