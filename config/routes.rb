129c99478dfbb38405ff7785fa9465d22a85266f
Jesse Parsons <savvyjesse@aol.com>
2020-06-08 12:15:30 -0400

    fix instance variable problem in view. issue was javascript_pack_tag in application layout - would render index action without the instance variables defined in flights#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :airports 
   resources :flights
-  get 'flights', to: "flights#index"
+  #get 'flights', to: "flights#index"
   root "flights#index"
 end
