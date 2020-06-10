45ce55674cc66671e32d036a60ac3ad459a97505
Jesse Parsons <savvyjesse@aol.com>
2020-06-10 18:11:31 -0400

    add date and passenger select, clean up view

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,6 +2,7 @@ class FlightsController < ApplicationController
   def index
     @flights = Flight.all
     @airport_options = Airport.all.map{ |a| [a.name, a.id]}
+    puts "params are #{params}"
     if (params[:from] && params[:to])
       @from = Airport.where(id: params[:from]).first
       @to = Airport.where(id: params[:to]).first
